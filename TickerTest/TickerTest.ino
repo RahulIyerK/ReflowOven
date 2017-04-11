372b93fbf5e2c5231becc3d0409013a0c1b3ddb2
RahulIyerK <rahul.iyer2@gmail.com>
2017-04-10 22:41:00 -0700

    updates to ticker test

--- TickerTest/TickerTest.ino
+++ TickerTest/TickerTest.ino
@@ -2,54 +2,70 @@
 
 #define LED1 12            
 
-#define PWM_PERIOD 1000 //milliseconds
+#define PWM_PERIOD 50 //milliseconds
 
-double dutyCycle = .75;
+double dutyCycle = 75; //percentage (min 0, max 100)
+bool signal_state = HIGH;
 
-unsigned long onTime = dutyCycle * PWM_PERIOD;
+Ticker ticker;
 
-Ticker flasher;
+int counter = 0; 
 
-
-
-void flash() //this function is called every PWM_PERIOD milliseconds
+void tick() //this function is called every PWM_PERIOD milliseconds
 {
-  wdt_disable();
-  unsigned long startMillis = millis();
-  unsigned long endMillis = startMillis + PWM_PERIOD;
-  
-  Serial.print(startMillis);
-  Serial.print(" ");
-  Serial.println(onTime);
-  
-  unsigned long curMillis = startMillis;
-  unsigned long stopMillis =  startMillis + onTime;
+//  wdt_disable();
+//  unsigned long startMillis = millis();
+//  unsigned long endMillis = startMillis + PWM_PERIOD;
+//  
+//  Serial.print(startMillis);
+//  Serial.print(" ");
+//  Serial.println(onTime);
+//  
+//  unsigned long curMillis = startMillis;
+//  unsigned long stopMillis =  startMillis + onTime;
+//
+//  while (curMillis < endMillis)
+//  {
+//      if (curMillis <= stopMillis)
+//      {
+//        Serial.println("writing HIGH");
+//        digitalWrite(LED1, HIGH);
+//      }
+//      else
+//      {
+//        Serial.println("writing LOW");
+//        digitalWrite(LED1, LOW);
+//      }
+//      
+//      curMillis++;
+//  }
+//  wdt_enable(WDTO_2S);
 
-  while (curMillis < endMillis)
+  digitalWrite(LED1, signal_state);
+
+
+  counter++;
+
+  Serial.println(counter);
+  if (counter == dutyCycle) //reached end of duty cycle, so we set state to LOW (it was HIGH until now)
   {
-      if (curMillis <= stopMillis)
-      {
-        Serial.println("writing HIGH");
-        digitalWrite(LED1, HIGH);
-      }
-      else
-      {
-        Serial.println("writing LOW");
-        digitalWrite(LED1, LOW);
-      }
-      
-      curMillis++;
+    signal_state = LOW;
   }
-  wdt_enable(WDTO_2S);
+  
+  if (counter == 100) //reached end of a period, so we reset everything
+  {
+    signal_state = HIGH;
+    counter = 0;
+  }
 }
 
 void setup() 
 {
   Serial.begin(115200);
   pinMode(LED1, OUTPUT);
-  
   digitalWrite(LED1, LOW);
-  flasher.attach_ms(PWM_PERIOD, flash);
+  
+  ticker.attach_ms(PWM_PERIOD, tick);
   
 }
 
