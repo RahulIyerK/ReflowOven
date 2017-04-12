a118d9a22f03cecc83e4aae249f8466664370b08
RahulIyerK <rahul.iyer2@gmail.com>
2017-04-11 19:29:06 -0700

    more updates to ticker

--- TickerTest/TickerTest.ino
+++ TickerTest/TickerTest.ino
@@ -2,9 +2,9 @@
 
 #define LED1 12
 
-int TICK_LENGTH = 15; //milliseconds
+int TICK_LENGTH = 20; //milliseconds
 
-double dutyCycle = 50; //percentage (min 0, max 100)
+uint8_t dutyCycle = 10; //percentage (min 0, max 100)
 
 Ticker ticker;
 
@@ -12,7 +12,7 @@ int counter = 0;
 
 void tick() //this function is called every TICK_LENGTH milliseconds
 {
-  if (counter == 100) //reached end of a period, so we reset everything
+  if (counter == 255) //reached end of a period, so we reset everything
   {
     counter = 0;
   }
@@ -21,7 +21,7 @@ void tick() //this function is called every TICK_LENGTH milliseconds
   
   counter++;
 
-  Serial.println(counter);  
+//  Serial.println(counter);  
 }
 
 void setup()
@@ -30,7 +30,7 @@ void setup()
   Serial.begin(115200);
   pinMode(LED1, OUTPUT);
 
-  Serial.println("hi");
+  //Serial.println("hi");
   ticker.attach_ms(TICK_LENGTH, tick);
 }
 
