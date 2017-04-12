80100912d92bf6e877c86fef6b3a272b4d263d11
RahulIyerK <rahul.iyer2@gmail.com>
2017-04-11 19:06:47 -0700

    more modifications to Ticker

--- TickerTest/TickerTest.ino
+++ TickerTest/TickerTest.ino
@@ -1,78 +1,40 @@
 #include <Ticker.h>
 
-#define LED1 12            
+#define LED1 12
 
-#define PWM_PERIOD 50 //milliseconds
+int TICK_LENGTH = 15; //milliseconds
 
-double dutyCycle = 75; //percentage (min 0, max 100)
-bool signal_state = HIGH;
+double dutyCycle = 50; //percentage (min 0, max 100)
 
 Ticker ticker;
 
-int counter = 0; 
+int counter = 0;
 
-void tick() //this function is called every PWM_PERIOD milliseconds
+void tick() //this function is called every TICK_LENGTH milliseconds
 {
-//  wdt_disable();
-//  unsigned long startMillis = millis();
-//  unsigned long endMillis = startMillis + PWM_PERIOD;
-//  
-//  Serial.print(startMillis);
-//  Serial.print(" ");
-//  Serial.println(onTime);
-//  
-//  unsigned long curMillis = startMillis;
-//  unsigned long stopMillis =  startMillis + onTime;
-//
-//  while (curMillis < endMillis)
-//  {
-//      if (curMillis <= stopMillis)
-//      {
-//        Serial.println("writing HIGH");
-//        digitalWrite(LED1, HIGH);
-//      }
-//      else
-//      {
-//        Serial.println("writing LOW");
-//        digitalWrite(LED1, LOW);
-//      }
-//      
-//      curMillis++;
-//  }
-//  wdt_enable(WDTO_2S);
-
-  digitalWrite(LED1, signal_state);
-
-
-  counter++;
-
-  Serial.println(counter);
-  if (counter == dutyCycle) //reached end of duty cycle, so we set state to LOW (it was HIGH until now)
-  {
-    signal_state = LOW;
-  }
-  
   if (counter == 100) //reached end of a period, so we reset everything
   {
-    signal_state = HIGH;
     counter = 0;
   }
+  
+  digitalWrite(LED1, (counter < dutyCycle));
+  
+  counter++;
+
+  Serial.println(counter);  
 }
 
-void setup() 
+void setup()
 {
+  
   Serial.begin(115200);
   pinMode(LED1, OUTPUT);
-  digitalWrite(LED1, LOW);
-  
-  ticker.attach_ms(PWM_PERIOD, tick);
-  
+
+  Serial.println("hi");
+  ticker.attach_ms(TICK_LENGTH, tick);
 }
 
-void loop() 
+void loop()
 {
-//  digitalWrite(LED1, HIGH);
-//  delay (250);
-//  digitalWrite(LED1, LOW);
-//  delay (250);
+  //wat
 }
