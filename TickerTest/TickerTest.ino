f999e045b06b35ec03786d6bfa46c75c6c1ad977
RahulIyerK <rahul.iyer2@gmail.com>
2017-04-11 20:55:15 -0700

    more updates

--- TickerTest/TickerTest.ino
+++ TickerTest/TickerTest.ino
@@ -1,6 +1,12 @@
 #include <Ticker.h>
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
 
-#define LED1 12
+#define LED1 4
+
+
+#define MAXCS   2
+Adafruit_MAX31855 thermocouple(MAXCS);
 
 int TICK_LENGTH = 20; //milliseconds
 
@@ -21,13 +27,19 @@ void tick() //this function is called every TICK_LENGTH milliseconds
   
   counter++;
 
-//  Serial.println(counter);  
+Serial.println(counter);  
 }
 
 void setup()
 {
-  
   Serial.begin(115200);
+  while (!Serial) delay(1); // wait for Serial on Leonardo/Zero, etc
+  
+  Serial.println("MAX31855 test");
+  // wait for MAX chip to stabilize
+  delay(500); 
+  
+  
   pinMode(LED1, OUTPUT);
 
   //Serial.println("hi");
@@ -36,5 +48,29 @@ void setup()
 
 void loop()
 {
-  //wat
+// basic readout test, just print the current temp
+ Serial.print("Internal Temp = ");
+ Serial.println(thermocouple.readInternal());
+ Serial.println(millis());
+
+ double c = thermocouple.readCelsius();
+ if (isnan(c)) {
+   Serial.println("Something wrong with thermocouple!");
+ } else {
+   Serial.print("C = "); 
+  Serial.println(c);
 }
+ Serial.print("F = ");
+ Serial.println(thermocouple.readFarenheit());
+
+ delay(1000);
+}
+
+
+
+
+
+
+
+
+
